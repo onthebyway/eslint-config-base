73c066dd471e9a77aba6738c531a0c312737fffd
David Rainer <david@byway.at>
2023-04-14 14:18:35 +0200

    remove wrong packages

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,14 +1,14 @@
 module.exports = {
   // eslint-configs
   extends: [
-    "airbnb",
-    "plugin:prettier/recommended",
-    "eslint:recommended",
-    "plugin:import/errors",
-    "plugin:prettier/recommended",
+    'airbnb',
+    'plugin:prettier/recommended',
+    'eslint:recommended',
+    'plugin:import/errors',
+    'plugin:prettier/recommended',
   ],
   // eslint-plugins
-  plugins: ["simple-import-sort", "prettier"],
+  plugins: ['prettier'],
   env: {
     browser: true,
     commonjs: true,
@@ -18,26 +18,23 @@ module.exports = {
   },
   // enabling/disabling/changing level of rules
   rules: {
-    "no-undef": "error",
-    "no-var": "error",
-    "prefer-const": "error",
-    "no-console": ["warn", { allow: ["warn", "error"] }],
-    "import/no-default-export": "warn",
-    "prettier/prettier": [
-      "error",
+    'no-undef': 'error',
+    'no-var': 'error',
+    'prefer-const': 'error',
+    'no-console': 'off',
+    'import/no-default-export': 'warn',
+    'prettier/prettier': [
+      'error',
       // configure Prettier for ESLint
       {
-        arrowParens: "avoid",
+        arrowParens: 'avoid',
         printWidth: 120,
         singleQuote: true,
-        trailingComma: "all",
-        singleQuote: true,
-        arrowParens: "always",
-        tabWidth: 4,
+        trailingComma: 'all',
+        tabWidth: 2,
         singleAttributePerLine: true,
         semi: true,
       },
     ],
-    radix: "off",
   },
 };
